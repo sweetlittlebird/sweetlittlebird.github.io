e14d2d7151e9c47f32eb88a3a7f6acbed64f5209
Sweet Little Bird <mysweetlittlebluebird@gmail.com>
2024-06-30 02:06:05 +0900

    3주차 추가

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Initialize Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-EWGY9N8QXY');
+
+// Initialize Disqus
+if (document.getElementById('disqus_thread') && location.hostname !== 'localhost') {
+    (function () { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://sweet-little-bird-1.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+}
