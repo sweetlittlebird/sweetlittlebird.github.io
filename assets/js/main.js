ec3956a99397ccbdf8f456fb11fb74467aede3e0
Sweet Little Bird <171835816+sweetlittlebird@users.noreply.github.com>
2024-07-14 02:56:40 +0900

    5주차 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,48 @@
-// Initialize Google Analytics
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-EWGY9N8QXY');
+(function (jQuery, $) {
+    if (location.hostname !== 'localhost') {
+        // Initialize Google Analytics
+        window.dataLayer = window.dataLayer || [];
 
-// Initialize Disqus
-if (document.getElementById('disqus_thread') && location.hostname !== 'localhost') {
-    (function () { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://sweet-little-bird-1.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
-}
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'G-EWGY9N8QXY');
+
+        // Initialize Disqus
+        if (document.getElementById('disqus_thread') && location.hostname !== 'localhost') {
+            (function () { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://sweet-little-bird-1.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        }
+    }
+
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        var hash = $(this).attr('href');
+        var hashTxt = decodeURIComponent(hash.substring(1));
+        var elem = document.getElementById(hashTxt);
+
+        if (! elem) {
+            return;
+        }
+
+        var offset = $(elem).offset();
+
+        if (! offset) {
+            return;
+        }
+
+        $('html, body').animate({
+            scrollTop: offset.top
+        });
+
+        location.hash = hash;
+
+        e.preventDefault();
+        e.stopPropagation();
+    });
+})(jQuery, jQuery);
