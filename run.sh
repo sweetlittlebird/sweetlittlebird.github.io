09b8390e862d9b6ad598d7de2f69f7fcee297d85
Sweet Little Bird <171835816+sweetlittlebird@users.noreply.github.com>
2025-08-17 01:37:16 +0900

    Cilium 5주차 내용 추가

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #! /bin/sh
-bundle exec jekyll serve --livereload
+
+# bundle exec jekyll serve --livereload
+
+# bundle exec jekyll serve --livereload --limit_posts 5 --future -I
+
+bundle exec jekyll serve --livereload --future 
